87cc238b775d1a01f3081d23839a24c2edbd9628
Justin Nguyen <jnguyenblue2804@gmail.com>
2023-02-11 19:45:50 -0600

    added gitignore

---
+++ numerical_methods/CMakeFiles/euler_methods.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/euler_methods.dir/src/euler_methods.cpp.o"
+  "euler_methods"
+  "euler_methods.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/euler_methods.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
